d045a5da2f7bbb9a8f5968e36326a1ce272a9ac5
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-06 19:51:50 +0700

    add h1,h2,inputs and copy btn

--- gradient-bg-react/src/App.jsx
+++ gradient-bg-react/src/App.jsx
@@ -2,7 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h1 className="main-title">Background Generator</h1>
+      <input type="color" className="color1" />
+      <input type="color" className="color2" />
+      <h2>Current css background</h2>
+      <div className="wrapper">
+        <span className="gradient-text"></span>
+        <img src="/clone.png" alt="clone button" />
+      </div>
+    </>
+  );
 }
 
 export default App;
