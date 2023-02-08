e48d5dc434d1e113f1281054bd403252d8783fb3
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-08 17:26:29 +0700

    implement changing colors, set gradient text and tooltip

--- gradient-bg-react/src/App.jsx
+++ gradient-bg-react/src/App.jsx
@@ -2,15 +2,76 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [colorOne, setColorOne] = useState("#3148db");
+  const [colorTwo, setColorTwo] = useState("#ffff00");
+  const [gradientText, setGradientText] = useState("");
+  const [isHidden, setIsHidden] = useState(true);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const changeColorOne = (e) => {
+    setColorOne(e.target.value);
+    changeGradient(e.target.value, colorTwo);
+  };
+
+  const changeColorTwo = (e) => {
+    setColorTwo(e.target.value);
+    changeGradient(colorOne, e.target.value);
+  };
+
+  const changeGradient = (colorOne, colorTwo) => {
+    document.documentElement.style.setProperty("--beginGradient", colorOne);
+    document.documentElement.style.setProperty("--endGradient", colorTwo);
+    changeGradientText();
+  };
+
+  const changeGradientText = () => {
+    const indexOfRepeat = getComputedStyle(document.body).background.indexOf(
+      "repeat"
+    );
+    const transformedText = getComputedStyle(document.body).background.slice(
+      17,
+      indexOfRepeat
+    );
+    setGradientText(transformedText);
+    setIsHidden(false);
+  };
+
+  const copyTextToClipboard = () => {
+    navigator.clipboard.writeText(gradientText);
+    setIsCopied(true);
+  };
+
   return (
     <>
       <h1 className="main-title">Background Generator</h1>
-      <input type="color" className="color1" />
-      <input type="color" className="color2" />
+      <input
+        type="color"
+        className="color1"
+        value={colorOne}
+        onChange={changeColorOne}
+      />
+      <input
+        type="color"
+        className="color2"
+        value={colorTwo}
+        onChange={changeColorTwo}
+      />
       <h2>Current css background</h2>
       <div className="wrapper">
-        <span className="gradient-text"></span>
-        <img src="/clone.png" alt="clone button" />
+        <h3 className="gradient-text">{gradientText}</h3>
+        <div className="tooltip">
+          <span className="tooltiptext" id="myTooltip">
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </span>
+          {!isHidden && (
+            <img
+              src="/clone.png"
+              alt="clone button"
+              className="copy-btn"
+              onClick={copyTextToClipboard}
+            />
+          )}
+        </div>
       </div>
     </>
   );
